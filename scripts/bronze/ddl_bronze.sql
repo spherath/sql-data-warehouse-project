1916d815a58fab91a0b6001c55e91ecf48a38af1
Sumudu Prasanna Herath <90689220+spherath@users.noreply.github.com>
2026-06-22 16:13:07 +1200

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,75 @@
+/*
+===================================================================================================
+DDL Script: Create Bronze Tebles
+===================================================================================================
+Script Purpose:
+    This script create table in the bronze table in the bronze scheema, droding exsisting tables
+    if they are already exsists.
+    Run this script to re-define the DDL structure of 'bronze' tables.
+===================================================================================================
+*/
+
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info; --t SQL logic
+CREATE TABLE bronze.crm_cust_info (
+	cst_id INT,
+	cst_key	NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE
+);
+
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info; --t SQL logic
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key	NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
+);
+
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details; --t SQL logic
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales  INT,
+	sls_quantity INT,
+	sls_price INT
+);
+
+
+-- tables generated from ERP
+
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101; --t SQL logic
+CREATE TABLE bronze.erp_loc_a101 (
+	cid NVARCHAR (50),
+	cntry NVARCHAR (50)
+);
+
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12; --t SQL logic
+CREATE TABLE bronze.erp_cust_az12 (
+	cid NVARCHAR (50),
+	bdate DATE,
+	gen NVARCHAR (50)
+);
+
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2; --t SQL logic
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+	id NVARCHAR (50),
+	cat NVARCHAR (50),
+	subcat NVARCHAR (50),
+	maintenance NVARCHAR (50)
+);
