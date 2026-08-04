0326f838a9ec422581d69834cef8cdd3b6493496
Sumudu Prasanna Herath <90689220+spherath@users.noreply.github.com>
2026-08-04 20:38:28 +1200

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -83,7 +83,7 @@ SELECT
 	sd.sls_ship_dt 		AS shipping_date,
 	sd.sls_due_dt 		AS due_date,
 	sd.sls_sales 		AS sales_amount,
-	sd.sls_quantity 	AS qualtity,
+	sd.sls_quantity 	AS quantity,
 	sd.sls_price 		AS price
 FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
